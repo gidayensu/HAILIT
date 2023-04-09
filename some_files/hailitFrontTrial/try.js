39523e38f1650e87ea0a7502072c6ab478c5a6b4
giadyensukwabi <thurgid@gmail.com>
2023-04-09 20:48:06 +0000

    removed additional redudant files

--- some_files/hailitFrontTrial/try.js
+++
@@ -1,8 +0,0 @@
-const myObj = {
-    customer_id: '1',
-    password: 'sdgs'
-}
-
-const {customer_id, password} = myObj;
-const passwordPlusId = [customer_id, password]
-console.log(passwordPlusId)
