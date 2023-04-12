129306570c7f9e4beaf83d893abbe5599bd63076
giadyensukwabi <thurgid@gmail.com>
2023-04-12 03:05:11 +0000

    App.js updated to make the inclusion of new routes maintainable

--- hailit_backend/App.js
+++ hailit_backend/App.js
@@ -20,19 +20,19 @@ app.use(session({
     saveUninitialized: false
 }))
 
-const v1CarDriverRoutes = require('./src/v1/routes/carDriver.routes')
-const v1CustomerRoutes = require('./src/v1/routes/customer.routes')
-const v1MotorRiderRoutes = require('./src/v1/routes/motorRider.routes')
-const v1OrderRoutes = require('./src/v1/routes/order.routes')
-const v1UserRoutes = require('./src/v1/routes/user.routes')
-const v1VehicleRoutes = require('./src/v1/routes/vehicle.routes')
-
-app.use('/api/v1/carDriver', v1CarDriverRoutes.router)
-app.use('/api/v1/customer', v1CustomerRoutes.router)
-app.use('/api/v1/motorRider', v1MotorRiderRoutes.router)
-app.use('/api/v1/order', v1OrderRoutes.router)
-app.use('/api/v1/user', v1UserRoutes.router)
-app.use('/api/v1/vehicle', v1VehicleRoutes.router)
+const v1Routes = [
+    { path: '/api/v1/carDriver', route: require('./src/v1/routes/carDriver.routes').router },
+    { path: '/api/v1/customer', route: require('./src/v1/routes/customer.routes').router },
+    { path: '/api/v1/motorRider', route: require('./src/v1/routes/motorRider.routes').router },
+    { path: '/api/v1/order', route: require('./src/v1/routes/order.routes').router },
+    { path: '/api/v1/user', route: require('./src/v1/routes/user.routes').router },
+    { path: '/api/v1/vehicle', route: require('./src/v1/routes/vehicle.routes').router }
+  ];
+  
+  for (const v1Route of v1Routes) {
+    app.use(v1Route.path, v1Route.route);
+  }
+  
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
